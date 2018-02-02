31db9f4f43c92bd040bbfa5d6fb23d99645dd5b5
LJGomez <tofilagman@gmail.com>
2018-02-02 16:14:54 +0800

    Initial Commit

---
+++ app/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+export class AppComponent extends Component {
+
+
+    constructor(args) {
+        super(args);
+    }
+
+    render() {
+        return (
+            <div>
+                hello react python d
+                </div>
+        )
+    }
+
+
+}
---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import { AppComponent } from './components/app';
+ 
+ReactDOM.render(
+    <AppComponent />,
+    document.getElementById('app')
+);
